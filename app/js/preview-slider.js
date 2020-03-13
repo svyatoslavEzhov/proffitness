bf51c912fe5111f2b58d53bbdb9f041bfc8dbf0a
bohdan993 <bodhan1trush@gmail.com>
2020-03-13 09:56:07 +0200

    13

--- app/js/preview-slider.js
+++ app/js/preview-slider.js
@@ -13,12 +13,12 @@ export default function() {
     }).on('afterChange', function(event, slick, currentSlide, nextSlide) {
 
         $(slick.$slides).find('.preview__slide-img_right, .preview__slide-img_left').removeClass('animated');
-        $(slick.$slides).find('.preview__slide-title, .preview__slide-text, .preview__slide .main-btn').removeClass('animated');
+        $(slick.$slides).find('.preview__slide-title, .preview__slide-text, .preview__slide .main-btn, .preview__slide-list').removeClass('animated');
         $(slick.$slides[currentSlide]).find('.preview__slide-img_right, .preview__slide-img_left').addClass('animated');
-        $(slick.$slides[currentSlide]).find('.preview__slide-title, .preview__slide-text, .preview__slide .main-btn').addClass('animated');
+        $(slick.$slides[currentSlide]).find('.preview__slide-title, .preview__slide-text, .preview__slide .main-btn, .preview__slide-list').addClass('animated');
     });
 
-    $('.preview__slide').eq(1).find('.preview__slide-title, .preview__slide-text, .main-btn, .preview__slide-img_right, .preview__slide-img_left').addClass('animated');
+    $('.preview__slide').eq(1).find('.preview__slide-title, .preview__slide-text, .main-btn, .preview__slide-img_right, .preview__slide-img_left, .preview__slide-list').addClass('animated');
 
 
 }
