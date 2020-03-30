6447f14a4592b1ac2bc9bf626e60e488fba9668e
BohdanT <bohdan1trush@gmail.com>
2020-03-31 02:31:40 +0300

    31.03.2020 2:31

--- app/js/filter_more.js
+++ app/js/filter_more.js
@@ -1,15 +1,15 @@
 import { log } from './main'
 
 export default function() {
-    let filter = $('.top__filters-list-item.active').attr('data-filter');
+    // let filter = $('.top__filters-list-item.active').attr('data-filter');
     $('.filter_ca__button').on('click', showMore);
 
     function showMore(e) {
         e.preventDefault();
 
-        $(`.unvisible-card.${filter}:lt(3)`).fadeIn(300).removeClass('unvisible-card');
+        $(`.unvisible-card:lt(3)`).fadeIn(300).css({display: 'flex'}).removeClass('unvisible-card');
 
-        if ($(`.unvisible-card.${filter}`).length <= 0) {
+        if ($(`.unvisible-card`).length <= 0) {
             $('.filter_ca__button').hide();
         }
     }
